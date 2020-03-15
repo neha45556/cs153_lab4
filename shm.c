766e08fc130da84af0e430d003bbe8788c1f68ba
Neha Gupta <ngupt009@sledge.cs.ucr.edu>
2020-03-14 23:20:56 -0700

    retry

--- shm.c
+++ shm.c
@@ -29,11 +29,35 @@ void shminit() {
 }
 
 int shm_open(int id, char **pointer) {
-
+int i;
 //you write this
+	acquire(&(shm_table.lock));
+	for(i = 0; i < 64; i++){
+		if(shm_table.shm_pages[i].id == id){
+			mappages(myproc()->pgdir,(void *)PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W | PTE_U);
+			shm_table.shm_pages[i].refcnt++;	
+			*pointer = (char *)PGROUNDUP(myproc()->sz);
+			myproc()->sz += PGSIZE;	
+			release(&(shm_table.lock));
+			return 0;
+		}
+	}
+	for(i = 0; i < 64; i++){
+		if(shm_table.shm_pages[i].id == 0){
+			shm_table.shm_pages[i].id = id;		
+			shm_table.shm_pages[i].frame = kalloc();
+			shm_table.shm_pages[i].refcnt = 1;
+			memset(shm_table.shm_pages[i].frame, 0, PGSIZE);
+			mappages(myproc()->pgdir, (void *)PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W | PTE_U);
+			*pointer = (char *)PGROUNDUP(myproc()->sz);
+			myproc()->sz += PGSIZE;
+			release(&(shm_table.lock));
+			return 0;
+		}
+	}
 
 
-
+release(&(shm_table.lock));
 
 return 0; //added to remove compiler warning -- you should decide what to return
 }
@@ -41,6 +65,19 @@ return 0; //added to remove compiler warning -- you should decide what to return
 
 int shm_close(int id) {
 //you write this too!
+int i;
+acquire(&(shm_table.lock));
+for(i = 0; i < 64; i++){
+	if(shm_table.shm_pages[i].id == id){
+		shm_table.shm_pages[i].refcnt--;
+		if(shm_table.shm_pages[i].refcnt == 0){
+			shm_table.shm_pages[i].id = 0;
+			shm_table.shm_pages[i].frame = 0;
+			shm_table.shm_pages[i].refcnt = 0;
+		}
+	}
+}
+release(&(shm_table.lock));
 
 
 
